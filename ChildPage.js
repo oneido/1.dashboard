51845f2a43afe420961304597bdc43bece044c6c
ido <44771235+oneido@users.noreply.github.com>
2021-06-15 07:34:42 +0900

    Update ChildPage.js

--- ChildPage.js
+++ ChildPage.js
@@ -1,5 +1,5 @@
 function receiveMessage(message) {
-    if (message.origin == "https://oneido.github.io/1.dashboard/dashBoard.html") {
+    if (message.origin == "https://oneido.github.io") {
         
         console.log(JSON.parse(message.data));
         [name, ...list] = JSON.parse(message.data);
